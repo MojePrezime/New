681d2c7b393e830f4d90f33b1085dac79df2467d
MojePrezime <MojePrezime>
2022-07-07 23:18:07 -0700

    ApexSQL Build commit

---
+++ ParentFolder/SubFolder/Users/##MS_AgentSigningCertificate##.sql
@@ -0,0 +1,3 @@
+CREATE USER [##MS_AgentSigningCertificate##]
+	FOR CERTIFICATE [##MS_AgentSigningCertificate##]
+GO
---
+++ ParentFolder/SubFolder/Users/##MS_PolicyEventProcessingLogin##.sql
@@ -0,0 +1,4 @@
+CREATE USER [##MS_PolicyEventProcessingLogin##]
+	FOR LOGIN [##MS_PolicyEventProcessingLogin##]
+	WITH DEFAULT_SCHEMA = [dbo]
+GO
